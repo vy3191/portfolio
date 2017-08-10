6595117b18e45beca26faa9a4591691c651679d8
vy3191 <vy3191@gmail.com>
2017-08-09 23:22:00 -0400

    Modified particles js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 window.onload = function() {
    Particles.init({
-     selector: '.background'
+     selector: '.background',
+     maxParticles: 40,
+     color: '#0000FF',
+     minDistance: 60,
+     sizeVariations: 4,
+     connectParticles: true,
+
    });
  };
+
 
 
 
