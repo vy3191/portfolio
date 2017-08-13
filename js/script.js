d29572567a752225f2eba9decb3813c354ef88dc
vy3191 <vy3191@gmail.com>
2017-08-13 16:22:26 -0400

    Particle js color changed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
    Particles.init({
      selector: '.background',
      maxParticles: 40,
-     color: '#1e90ff',
+     color: '#000080',
      minDistance: 60,
      sizeVariations: 4,
      connectParticles: true,
