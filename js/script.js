02be7018ae0c3e3eec1c91c196f8fdc78ea89f41
vy3191 <venkyafhs@gmail.com>
2017-09-18 09:30:55 -0400

    Modified particle js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-window.onload = function() {
-   Particles.init({
-     selector: '.background',
-     maxParticles: 30,
-     color: '#000070',
-     minDistance: 38,
-     sizeVariations: 1,
-     connectParticles: true,
+// window.onload = function() {
+//    Particles.init({
+//      selector: '.background',
+//      maxParticles: 30,
+//      color: '#000070',
+//      minDistance: 38,
+//      sizeVariations: 1,
+//      connectParticles: true,
 
-   });
- };
+//    });
+//  };
 
 
 
