0508d1ae843fc97e27024e31a202bf3b6b33bb6e
vy3191 <venkyafhs@gmail.com>
2017-09-17 23:00:06 -0400

    Particle JS modified

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.onload = function() {
    Particles.init({
      selector: '.background',
-     maxParticles: 20,
-     color: '#000010',
+     maxParticles: 10,
+     color: '#0000670',
      minDistance: 30,
      sizeVariations: 1,
      connectParticles: false,
