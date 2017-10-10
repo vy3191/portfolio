fef41b035f91f3169968ae5cae2c139b82bc23fa
vy3191 <venkyafhs@gmail.com>
2017-10-10 03:54:16 -0400

    index.html modified

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-// window.onload = function() {
-//    Particles.init({
-//      selector: '.background',
-//      maxParticles: 30,
-//      color: '#000070',
-//      minDistance: 38,
-//      sizeVariations: 1,
-//      connectParticles: true,
+window.onload = function() {
+   Particles.init({
+     selector: '.background',
+     maxParticles: 40,
+     color: '#0000FF',
+     minDistance: 60,
+     sizeVariations: 1,
+     connectParticles: true,
 
-//    });
-//  };
+   });
+ };
+
+ const btn = document.querySelector('.btn');
+ const name = document.querySelector('#name');
+ const email = document.querySelector('#email');
+ const msg = document.querySelector('#message');
+ const phone = document.querySelector('#phone');
+ btn.addEventListener('click', (event) => {
+          event.preventDefault();
+          console.log('form submission stopped');
+          msg.value = '';
+          name.value = '';
+          email.value = '';
+          phone.value = '';
+
+ });
 
 
 
